095aceb816a0e89311fa1555210856da46067c29
Ugo Muhieddine <ugo.muhieddine@student-cs.fr>
2023-12-02 10:50:09 +0100

    feat: new style

---
+++ diamond_dashboard.py
@@ -0,0 +1,83 @@
+import streamlit as st
+import pandas as pd
+from pathlib import Path
+import plotly.express as px
+
+
+def main_diamond(carat_df):
+    diamond_df = carat_df[carat_df.gemstone == "diamond"].copy()
+
+    st.markdown(
+        f"""
+    ## Main information
+    Total number of diamonds analysed: **{len(diamond_df):,.0f}**
+
+    Total valued sold: **{diamond_df.PriceRealised.sum():,.0f} €**
+    """
+    )
+
+    st.markdown(
+        """
+    ### Diamonds sales over time
+    """
+    )
+    all_years = diamond_df.index.year.unique()
+
+    values = st.slider(
+        "Select the time you want to display",
+        all_years.min(),
+        all_years.max(),
+        (all_years.min(), all_years.max()),
+    )
+
+    bar_over_time = px.bar(
+        diamond_df.loc[str(values[0]) : str(values[1])]
+        .resample("1M")["PriceRealised"]
+        .sum(),
+        y="PriceRealised",
+        color_discrete_sequence=px.colors.sequential.RdBu,
+    )
+    bar_over_time.update_layout(dragmode=False)
+    st.plotly_chart(bar_over_time, use_container_width=True)
+
+    st.markdown(
+        """
+    ### Clarity analysis
+    """
+    )
+    clarity_grouped = (
+        diamond_df.groupby("clarity")
+        .agg(
+            counted=("clarity", "count"),
+            valued=("PriceRealised", "sum"),
+            mean_price_per_carat=("price_per_ct", "mean"),
+        )
+        .reset_index()
+    )
+
+    clarity_pie = px.pie(
+        clarity_grouped,
+        values="counted",
+        names="clarity",
+        title="Diamond Clarity Distribution",
+        color_discrete_sequence=px.colors.sequential.RdBu,
+    )
+
+    st.plotly_chart(clarity_pie, use_container_width=True)
+
+    bar_price_clarity = px.bar(
+        clarity_grouped.sort_values(by="mean_price_per_carat", ascending=False),
+        x="clarity",
+        y="mean_price_per_carat",
+        color="counted",
+        color_continuous_scale=px.colors.sequential.RdBu_r,
+    )
+
+    bar_price_clarity.update_layout(
+        title="Price of the different clarities",
+        yaxis=dict(title="Average €/carat"),
+        showlegend=False,
+    )
+    bar_price_clarity.update_coloraxes(showscale=False)
+    bar_price_clarity.update_layout(dragmode=False)
+    st.plotly_chart(bar_price_clarity, use_container_width=True)
--- my_st_code.py
+++ my_st_code.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 from pathlib import Path
 import plotly.express as px
-
+from diamond_dashboard import main_diamond
 
 DATA_PATH = Path("data")
-df = pd.read_pickle(DATA_PATH / "certif_one_gem_for_streamlit.pkl")
+df = pd.read_pickle(DATA_PATH / "certified_one_gem_processed_data.pkl")
 
 # preprocessing
 df = df[df.carat >= 1].copy()
@@ -21,71 +21,7 @@ is_diamond, is_gems, is_big_sales = st.tabs(
 )
 
 with is_diamond:
-
-    diamond_df = carat_df[carat_df.gemstone == "diamond"].copy()
-
-    st.markdown(
-        f"""
-    ## Main information
-    Total number of diamonds analysed: **{len(diamond_df):,.0f}**
-
-    Total valued sold: **{diamond_df.PriceRealised.sum():,.0f} €**
-    """
-    )
-
-    st.markdown(
-        """
-    ### Diamonds sales over time
-    """
-    )
-    bar_over_time = px.bar(
-        diamond_df.resample("1M")["PriceRealised"].sum(),
-        y="PriceRealised",
-        color_discrete_sequence=px.colors.sequential.RdBu,
-    )
-    st.plotly_chart(bar_over_time, use_container_width=True)
-
-    st.markdown(
-        """
-    ### Clarity analysis
-    """
-    )
-    clarity_grouped = (
-        diamond_df.groupby("clarity")
-        .agg(
-            counted=("clarity", "count"),
-            valued=("PriceRealised", "sum"),
-            mean_price_per_carat=("price_per_ct", "mean"),
-        )
-        .reset_index()
-    )
-
-    clarity_pie = px.pie(
-        clarity_grouped,
-        values="counted",
-        names="clarity",
-        title="Diamond Clarity Distribution",
-        color_discrete_sequence=px.colors.sequential.RdBu,
-    )
-
-    st.plotly_chart(clarity_pie, use_container_width=True)
-
-    bar_price_clarity = px.bar(
-        clarity_grouped.sort_values(by="mean_price_per_carat", ascending=False),
-        x="clarity",
-        y="mean_price_per_carat",
-        color="counted",
-        color_continuous_scale=px.colors.sequential.RdBu_r,
-    )
-
-    bar_price_clarity.update_layout(
-        title="Price of the different clarities",
-        yaxis=dict(title="Average €/carat"),
-        showlegend=False,
-    )
-    bar_price_clarity.update_coloraxes(showscale=False)
-
-    st.plotly_chart(bar_price_clarity, use_container_width=True)
+    main_diamond(carat_df)
 
 with is_gems:
     # do_monthly_balance(USERNAME)
